7aaad7b09f805897b82dda32f2d1d421504922c7
hamidms <hamidmsukardi@gmail.com>
2019-04-30 21:34:53 +0700

    gparted installation

---
+++ src/assets/bash/unetbootin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#isntaller untuk aplikasi UNetBootin
+
+sudo add-apt-repository ppa:gezakovacs/ppa
+sudo apt-get update
+sudo apt-get install unetbootin
---
+++ src/assets/bash/xdm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#isntaller untuk aplikasi XDM
+
+sudo add-apt-repository ppa:noobslab/apps
+sudo apt-get update
+sudo apt-get install xdman
